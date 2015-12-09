fc5e0bbad7ce56c34837180c18a9746fee2aee2a
Aske Hansen <aske@deeco.dk>
2015-12-09 14:16:47 +0100

    works now?

--- app/interactors/process_email.rb
+++ app/interactors/process_email.rb
@@ -2,8 +2,8 @@ class ProcessEmail
   include Interactor
 
   def call
-    email = context.msg['from_email']
-    name = context.msg['from_name']
+    from_email = context.msg['from_email']
+    from_name = context.msg['from_name']
     email = context.msg['email']
     attachments = context.msg['attachments'].to_a
 
@@ -11,12 +11,22 @@ class ProcessEmail
     user = User.find id
 
     attachments.each do |filename, attachment|
-      file = Tempfile.new filename
-      file.write attachment['content']
+      ext = MIME::Types[attachment['type']].first.extensions.first
+      tmpfile = Tempfile.new [filename, ".#{ext}"], encoding: 'ascii-8bit'
+
+      if attachment['base64']
+        tmpfile.write Base64.decode64(attachment['content'])
+      else
+        tmpfile.write attachment['content']
+      end
+
+
+      # weird behaviour where dinero wont accept the tmpfile
+      tmpfile.close
+      file = File.open tmpfile.path
 
       UploadPurchase.call file: file, organization_id: user.organization_id, api_key: user.api_key
-
-      UserMailer.upload_complete(email: email, name: name, filename: filename).deliver_now
+      UserMailer.upload_complete(email: from_email, name: from_name, filename: filename).deliver_now
     end
   end
 end
