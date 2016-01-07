929c8746520b4ae11e148789d1a66bd7be936c05
Aske Hansen <aske@deeco.dk>
2016-01-07 22:58:58 +0100

    send to dinero with postmark

--- app/controllers/postmark_controller.rb
+++ app/controllers/postmark_controller.rb
@@ -3,7 +3,35 @@ class PostmarkController < ApplicationController
 
   def create
     email = Postmark::Mitt.new(request.body.read)
-    Rails.logger.info email
+
+    filenames = email.attachments.map(&:file_name).join(', ')
+    Rails.logger.info "Processing email for #{message.from_email} with #{filenames}"
+
+    message = InboundMessage.new(
+      from_name:  email.from_name,
+      from_email: email.from_email,
+      email:      email.to_email,
+      subject:    email.subject
+    )
+
+    user = User.find(message.user_id)
+
+    attachments = email.attachments.map do |attachment|
+      InboundAttachment.new(attachment.file_name, attachment.read.read)
+    end
+
+    attachments.each do |attachment|
+      attachment.save!
+
+      CreatePurchaseJob.perform_later(
+        file_key:   attachment.key,
+        filename:   attachment.filename,
+        from_email: message.from_email,
+        from_name:  message.from_name,
+        subject:    message.subject,
+        user:       user
+      )
+    end
 
     head :ok
   end
