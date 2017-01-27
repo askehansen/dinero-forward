d9a6f590968ad4ccddade4e20b818b5cfe8274c5
Aske Hansen <aske@deeco.dk>
2017-01-27 11:28:56 +0100

    strip ids

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   validates_presence_of :organization_id, :api_key
+
+  before_save :strip_keys
+
+  private
+
+  def strip_keys
+    self.organization_id.strip!
+    self.api_key.strip!
+  end
+
 end
