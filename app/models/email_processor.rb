eba551ad77e8f07874c88ab7880f3d26ab6cf984
Aske Hansen <aske@deeco.dk>
2017-06-30 16:17:01 +0200

    sendgrid

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -4,55 +4,104 @@ class EmailProcessor
     @email = email
   end
 
+
   def process
     Rails.logger.info "Processing email for #{@email.from[:email]} with #{filenames}"
-    create_purchases if user
+
+    attachments.each do |attachment|
+      attachment.save!
+
+      CreatePurchaseJob.perform_later(
+        file_key:   attachment.key,
+        filename:   attachment.filename,
+        from_email: message.from_email,
+        from_name:  message.from_name,
+        subject:    message.subject,
+        user:       user
+      )
+    end
   end
 
   private
 
-    def create_purchases
-      attachments.each do |attachment|
-        create_purchase(attachment)
-      end
+  def attachments
+    @_attachments ||= @email.attachments.map do |file|
+      attachment_from_file(file)
     end
+  end
 
-    def attachments
-      @_attachments ||= @email.attachments.map do |file|
-        attachment = InboundAttachment.new(filename(file), file.read)
-        attachment.save!
-        attachment
-      end
-    end
+  def user
+    @_user ||= User.find(message.user_id)
+  end
 
-    def create_purchase(attachment)
-      CreatePurchaseJob.perform_later(
-        file_key:   attachment.key,
-        filename:   attachment.filename,
-        from_email: @email.from[:email],
-        from_name:  @email.from[:name],
-        subject:    @email.subject,
-        user:       user
-      )
-    end
+  def message
+    @_message ||= InboundMessage.new(
+      from_name:  @email.from[:name],
+      from_email: @email.from[:email],
+      email:      @email.to.first[:email],
+      subject:    @email.subject
+    )
+  end
 
-    def user
-      @user ||= begin
-        User.find(@email.to[:token])
-      rescue ActiveRecord::RecordNotFound
-        Rails.logger.info "User not found with id #{@email.to[:token]}"
-        nil
-      end
-    end
-
-    def filenames
-      @email.attachments.map do |file|
-        filename(file)
-      end
-    end
-
-    def filename(file)
+  def filenames
+    @_filenames ||= @email.attachments.map do |file|
       file.path.split('/').last
-    end
+    end.join(', ')
+  end
+
+  def attachment_from_file(file)
+    file_name = file.path.split('/').last
+    InboundAttachment.new(file_name, file.ready)
+  end
+
+  # LEGACY
+
+
+  #
+  # def create
+  #   email = Postmark::Mitt.new(request.body.read)
+  #
+  #   filenames = email.attachments.map(&:file_name).join(', ')
+  #   Rails.logger.info "Processing email for #{email.from_email} with #{filenames}"
+  #
+  #   message = InboundMessage.new(
+  #     from_name:  email.from_name,
+  #     from_email: email.from_email,
+  #     email:      email.to_email,
+  #     subject:    email.subject
+  #   )
+  #
+  #   user = User.find(message.user_id)
+  #
+  #   attachments = email.attachments.map do |attachment|
+  #     attachment_from_content(attachment)
+  #   end
+  #
+  #   attachments.each do |attachment|
+  #     attachment.save!
+  #
+  #     CreatePurchaseJob.perform_later(
+  #       file_key:   attachment.key,
+  #       filename:   attachment.filename,
+  #       from_email: message.from_email,
+  #       from_name:  message.from_name,
+  #       subject:    message.subject,
+  #       user:       user
+  #     )
+  #   end
+  #
+  #   head :ok
+  # end
+  #
+  #
+  # def attachment_from_content(attachment)
+  #   InboundAttachment.new(attachment.file_name, attachment.source['Content'], InboundAttachment::Base64Decoder.new)
+  # end
+  #
+  # def attachment_from_tempfile(attachment)
+  #   file = attachment.read
+  #   file.rewind
+  #   InboundAttachment.new(attachment.file_name, file.read)
+  # end
 
 end
