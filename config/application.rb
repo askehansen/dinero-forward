45929a4554234451b6345b5cd1a49428cc448dc2
Aske Hansen <aske@deeco.dk>
2017-08-16 16:00:02 +0200

    upgrade rails

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,6 @@ module DineroForward
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :da
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.autoload_paths << Rails.root.join('lib')
 
     config.active_job.queue_adapter = :sidekiq
