50d9c1fbca07316fe261d9c335c631b5f41d11aa
Aske Hansen <aske@deeco.dk>
2017-08-17 10:58:48 +0200

    fix

--- app/jobs/process_purchase_job.rb
+++ app/jobs/process_purchase_job.rb
@@ -13,10 +13,10 @@ class ProcessPurchaseJob < ApplicationJob
 
     if upload.success?
       purchase.processed!
-      UserMailer.upload_complete(email: purchase.from_email, name: purchase.from_name, filename: purchase.filename).deliver_later
+      UserMailer.upload_complete(email: purchase.message.from_email, name: purchase.message.from_name, filename: purchase.filename).deliver_later
     else
       purchase.failed!
-      UserMailer.upload_failed(email: purchase.from_email, name: purchase.from_name, filename: purchase.filename, error: upload.error).deliver_later
+      UserMailer.upload_failed(email: purchase.message.from_email, name: purchase.message.from_name, filename: purchase.filename, error: upload.error).deliver_later
     end
   end
 
